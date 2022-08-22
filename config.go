6c5463de5388eef958fc382f3a7c15223e174bd9
v.chehov <adushkin08@gmail.com>
2022-08-22 17:18:18 +0300

    init commit

---
+++ config.go
@@ -0,0 +1,29 @@
+package main
+
+import (	
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log" 
+)
+
+var config = NewConfig()
+
+type Config struct {
+	Token      string   `yaml:"token"`
+}
+
+func NewConfig() *Config {
+
+	c := new(Config)
+
+	bytes, err := ioutil.ReadFile("./app.yaml")
+	if err != nil {
+		log.Printf("Error load read config %v", err)
+	}
+	err = yaml.Unmarshal(bytes, &c)
+	if err != nil {
+		log.Printf("Error unmarhsl %v", err)
+	}
+
+	return c
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module linker-bot
+
+go 1.17
+
+require (
+	github.com/sirupsen/logrus v1.9.0
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/Syfaro/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
+
+require (
+	github.com/vbchekhov/skeleton v0.0.0-20201116185019-7c95220e1b43
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
